16c8bc469e16a7e986ebf7e69e8666fb0ef010c7
madmmich <97074540+madmmich@users.noreply.github.com>
2022-03-03 10:38:26 -0800

    Add files via upload

--- snscrape-code-tinder.py
+++ snscrape-code-tinder.py
@@ -1,9 +1,17 @@
+import os
+import pandas as pd
 import csv
+import snscrape
 
-tinder-tweets = open('user-tweets-tinder.csv', 'r')
-emoji-tweets = csv.DictReader(tinder-tweets)
-tweet-content = []
+tweet_count = 100
+username = "tinder"
 
-for col in emoji-tweets:
-    tweet-content.append(col['month_number']) 
-print('Tweet body:', tweet-content)
+# Using OS library to call CLI commands in Python
+os.system("snscrape --jsonl --max-results {} twitter-search 'from:{}'> user-tweets.json".format(tweet_count, username))
+# Reads the json generated from the CLI command above and creates a pandas dataframe
+tweets_df1 = pd.read_json('user-tweets-tinder.json', lines=True)
+
+# Displays first 5 entries from dataframe
+tweets_df1.head()
+
+tweets_df1.to_csv('user-tweets-tinder.csv', sep=',', index=False)
