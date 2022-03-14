c008d10d2b43c5113f045b5876e8ed85686f8882
madmmich <97074540+madmmich@users.noreply.github.com>
2022-03-14 16:25:35 -0700

    Update snscrape-filter-arbys.py

--- code/snscrape-filter-arbys.py
+++ code/snscrape-filter-arbys.py
@@ -6,12 +6,9 @@ import snscrape
 tweet_count = 100
 username = "arbys"
 
-# Using OS library to call CLI commands in Python
 os.system("snscrape --jsonl --max-results {} twitter-search 'from:{}'> user-tweets-arbys.json".format(tweet_count, username))
-# Reads the json generated from the CLI command above and creates a pandas dataframe
 tweets_df1 = pd.read_json('user-tweets-arbys.json', lines=True)
 
-# Displays first 5 entries from dataframe
 tweets_df1.head()
 
 tweets_df1.to_csv('user-tweets-arbys.csv', sep=',', index=False)
